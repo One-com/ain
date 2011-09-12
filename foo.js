e65170cc2d8f496506e99b111ac47bf64ec3f10a
Andreas Lind Petersen <andreas@one.com>
2011-09-12 14:19:33 +0200

    Removed test file.

--- foo.js
+++
@@ -1,4 +0,0 @@
-var logger = require('./index.js').set('thetag', 'user', 'error', 'unix', '/dev/log');
-
-logger.warn("this is a warning, shouldn't be logged!");
-logger.error("this is an ERROR, should be logged!");
